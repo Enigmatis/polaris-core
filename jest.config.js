e7d9309883e6835442f86bd7bb0432e62747e18c
osher-sade <osher.sade@gmail.com>
2019-10-31 11:03:58 +0200

    ci: added travis support

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    clearMocks: true,
+    moduleFileExtensions: [
+        "ts",
+        "js",
+    ],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [
+        "**/*.test.ts"
+    ],
+};
