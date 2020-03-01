f836b078e33f88ca1d4aa1028de7776f7d0e8cbe
DoctorVoid <osher.sade@gmail.com>
2020-03-01 17:18:57 +0200

    fix(extensions): fixed extensions being overridden

--- src/extensions/extensions-listener.ts
+++ src/extensions/extensions-listener.ts
@@ -1,23 +1,23 @@
 import { PolarisGraphQLContext } from '@enigmatis/polaris-common';
 import { PolarisGraphQLLogger } from '@enigmatis/polaris-graphql-logger';
-import { GraphQLRequestListener } from 'apollo-server-plugin-base';
+import { GraphQLRequestContext, GraphQLRequestListener } from 'apollo-server-plugin-base';
 
-export class ExtensionsListener implements GraphQLRequestListener {
+export class ExtensionsListener implements GraphQLRequestListener<PolarisGraphQLContext> {
     public readonly logger: any;
 
     constructor(logger: PolarisGraphQLLogger) {
         this.logger = logger;
     }
 
-    public async willSendResponse(requestContext: any) {
-        const {
-            context,
-            response,
-        }: { context: PolarisGraphQLContext; response: any } = requestContext;
+    public async willSendResponse(
+        requestContext: GraphQLRequestContext<PolarisGraphQLContext> &
+            Required<Pick<GraphQLRequestContext<PolarisGraphQLContext>, 'metrics' | 'response'>>,
+    ): Promise<void> {
+        const { context, response } = requestContext;
+
         if (context.returnedExtensions) {
             this.logger.debug('extensions were set to response');
-            response.extensions = context.returnedExtensions;
+            response.extensions = { ...response.extensions, ...context.returnedExtensions };
         }
-        return requestContext;
     }
 }
