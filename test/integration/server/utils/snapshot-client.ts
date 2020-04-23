7b324be5ec39b4ee29a3dc3a4f1118d84da54c6a
Arik F <furmanmail@gmail.com>
2020-04-23 12:17:44 +0300

    fix: review fixes

--- test/integration/server/utils/snapshot-client.ts
+++ test/integration/server/utils/snapshot-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import * as polarisProperties from '../resources/polaris-properties.json';
+import { port } from '../resources/polaris-properties.json';
 
-export const url = `http://localhost:${polarisProperties.port}/snapshot`;
+export const url = `http://localhost:${port}/snapshot`;
 
 export const snapshotRequest = async (snapshotId: string) => {
     return axios(url + '?id=' + snapshotId, { method: 'get' });
