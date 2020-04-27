ddd7dad6570224caf30e963ce1f1e6c966e31135
itaykl <itaykl123@gmail.com>
2020-04-27 17:31:47 +0300

    test: little changes.

--- test/integration/server/dal/connection-manager.ts
+++ test/integration/server/dal/connection-manager.ts
@@ -1,32 +1,6 @@
-import {
-    ConnectionOptions,
-    createPolarisConnection,
-    PolarisConnection,
-} from '@enigmatis/polaris-typeorm';
+import { ConnectionOptions, createPolarisConnection } from '@enigmatis/polaris-typeorm';
 import { polarisGraphQLLogger } from '../utils/logger';
 
 export async function initConnection(connectionOptions: ConnectionOptions) {
-    const connection = await createPolarisConnection(
-        connectionOptions,
-        polarisGraphQLLogger as any,
-    );
-    await deleteTables(connection);
-}
-export async function deleteTables(connection: PolarisConnection): Promise<void> {
-    const tables = ['book', 'author', 'data_version', 'snapshot_page'];
-    for (const table of tables) {
-        if (connection) {
-            try {
-                const tableRepo = connection.getRepository(table);
-                const schema = tableRepo.metadata.schema;
-                const path = schema ? schema + '"."' + table : table;
-                await tableRepo.query('DELETE FROM "' + path + '";');
-            } catch (e) {
-                polarisGraphQLLogger.debug(
-                    "Couldn't delete table (might never existed)",
-                    {} as any,
-                );
-            }
-        }
-    }
+    await createPolarisConnection(connectionOptions, polarisGraphQLLogger as any);
 }
