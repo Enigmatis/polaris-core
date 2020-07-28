e6e75efe3dc1cf89dbd0ed594690a019daaf959c
Arik Furman <furmanmail@gmail.com>
2020-07-28 10:27:17 +0300

    fix: fixed test timing

--- test/integration/tests/snapshot-metadata-cleaning.test.ts
+++ test/integration/tests/snapshot-metadata-cleaning.test.ts
@@ -11,8 +11,8 @@ beforeEach(async () => {
         snapshotConfig: {
             autoSnapshot: true,
             maxPageSize: 3,
-            snapshotCleaningInterval: 1,
-            secondsToBeOutdated: 4,
+            snapshotCleaningInterval: 5,
+            secondsToBeOutdated: 13,
             entitiesAmountPerFetch: 50,
         },
     });
@@ -29,7 +29,7 @@ describe('snapshot metadata cleaned every interval', () => {
         });
         const snapshotMetadataId = paginatedResult.extensions.snapResponse.snapshotMetadataId;
         await waitUntilSnapshotRequestIsDone(snapshotMetadataId, 500);
-        await sleep(5000);
+        await sleep(20000);
         const metadataResponse = await metadataRequest(snapshotMetadataId);
         expect(metadataResponse.data).toBe('');
 
